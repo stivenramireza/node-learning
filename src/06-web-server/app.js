9e4264956ae74da50b522043f7744317eaa85079
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-10-10 21:25:21 -0500

    [ADD] Completed file

--- src/06-web-server/app.js
+++ src/06-web-server/app.js
@@ -6,12 +6,24 @@ const port = 8080;
 // Serve static content
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 app.get('/hello-world', (req, res) => {
     res.send('Hello World in its respective route');
 });
 
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
+});
+
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
+    res.sendFile(__dirname + '/public/back/404.html');
 });
 
 app.listen(port, () => {
