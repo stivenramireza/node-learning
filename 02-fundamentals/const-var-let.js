5a4b223674b65c5d7daae9a76fa244afad06f4f3
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-12-03 17:30:31 -0500

    [ADD] Web server

--- 02-fundamentals/const-var-let.js
+++
@@ -1,9 +0,0 @@
-const name = 'Wolverine';
-
-// eslint-disable-next-line no-constant-condition
-if (true) {
-    const name = 'Magneto';
-    // console.log(name);
-}
-
-console.log(name);
--- 07-rest-server/src/services/files.js
+++
@@ -1,8 +0,0 @@
-const { uploadFile } = require('../utils');
-
-const updateImage = async (model, files, collection) => {
-    model.img = await uploadFile(files, undefined, collection);
-    await model.save();
-};
-
-module.exports = { updateImage };
