9008376d5d26bd5e1a2befacaf7ea58779984aba
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-12-09 22:06:17 -0500

    [ADD] connect and disconnect socket events

--- 08-websocket-server/public/js/socket-client.js
+++ 08-websocket-server/public/js/socket-client.js
@@ -1,1 +1,17 @@
+// HTML references
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+});
