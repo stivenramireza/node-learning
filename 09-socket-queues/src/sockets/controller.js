d2a16427fa2014b678298ccf26d30ca266a8eef3
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-12-10 19:48:54 -0500

    [ADD] Events with the current status of tickets and attending a new ticket

--- 09-socket-queues/src/sockets/controller.js
+++ 09-socket-queues/src/sockets/controller.js
@@ -10,12 +10,27 @@ const socketController = (socket) => {
     });
 
     socket.emit('last-ticket', ticketControl.lastTicket);
+    socket.emit('pending-tickets', ticketControl.tickets.length);
+    socket.emit('current-status', ticketControl.lastTickets);
 
     socket.on('next-ticket', (payload, callback) => {
         const nextTicket = ticketControl.nextTicket();
         callback(nextTicket);
 
         // TODO: Notify a new pending ticket
+    });
+
+    socket.on('attend-ticket', ({ desk }, callback) => {
+        if (!desk) callback({ success: false, message: 'Desk is mandatory' });
+
+        const ticket = ticketControl.attendTicket(desk);
+        // TODO: Notify changes in the last tickets
+        socket.emit('pending-tickets', ticketControl.tickets.length);
+        socket.broadcast.emit('current-status', ticketControl.lastTickets);
+
+        if (!ticket) callback({ success: false, message: 'There are not more tickets' });
+
+        callback({ success: true, ticket, pendingTickets: ticketControl.tickets.length });
     });
 };
 
