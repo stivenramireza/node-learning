2731ad8763a96557a41b35cff61ef410a98237e5
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-12-28 16:17:19 -0500

    [ADD] Model to handle connected users and their messages

--- 10-socket-chat/src/models/index.js
+++ 10-socket-chat/src/models/index.js
@@ -2,10 +2,12 @@ const Category = require('./category');
 const Product = require('./product');
 const Role = require('./role');
 const User = require('./user');
+const Chat = require('./chat');
 
 module.exports = {
     Category,
     Product,
     Role,
     User,
+    Chat,
 };
